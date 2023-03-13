4ffb1d687e9b22f47b793a8c106d3ed51ed23216
MohamedSiddiq88 <124970161+MohamedSiddiq88@users.noreply.github.com>
2023-03-13 12:23:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function foo(){
+    document.body.style.backgroundColor="red";
+    document.body.style.color="white";
+   let fn= document.getElementById("fn").value;
+   let mn=document.getElementById("mn").value;
+   let ln= document.getElementById("ln").value;
+   let em=document.getElementById("email").value;
+    let pas=document.getElementById("password").value;
+console.log(fn);
+console.log(mn);
+console.log(ln);
+console.log(em);
+console.log(pas);
+
+}
